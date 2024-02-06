2b7683ede646d216f4f0329b2e1484ab8b4e1a04
Jayesh Bharadva <jbharadva@codal.com>
2024-02-06 18:07:21 +0530

    final changes and added readme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Counter from './component/Counter';
-import Navigation from './component/Navigation';
 function App() {
   return (
    <>
-    <Navigation />
     <Counter />
-    {/* <Count/> */}
-    {/* <First/> */}
    </>
   );
 }
